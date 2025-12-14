3a925b4311c237d1f24a3eee45ccd352f1d13411
Hans Philip Eide <hans@oddadigitalsystem.no>
2025-12-14 11:52:17 +0100

    Updated description in the getAxiosClient.

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -8,13 +8,13 @@ const api = new OpenAPIClientAxios({
 let clientInstance: Client | null = null
 
 export const getAxiosClient = async (): Promise<Client> => {
-  // Here's a quite basic implementation, we need to provide BC
-  // credentials and a bearer token her, that's a topic for
-  // another article :-)
+  // This is quite a basic implementation, we need to provide BC
+  // credentials and a bearer token here, that's a topic for
+  // another article, how do we authorize our app and provide
+  // the credentials? :-)
 
   if (!clientInstance) {
     clientInstance = await api.getClient<Client>()
-    clientInstance.defaults.baseURL = 'http://localhost:3000/'
   }
   return clientInstance
 }
